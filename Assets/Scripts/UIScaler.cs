d9a92aa175ab32099288e2fce0a97184311e8ec4
dk731 <demid.kaidalov@gmail.com>
2020-10-15 21:34:38 +0300

    Feature of UI Scaling
    only 16:9 ration supported currently

---
+++ Assets/Scripts/UIScaler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIScaler : MonoBehaviour
+{
+    private Vector2 originalSize = new Vector2(2540, 1440);
+    private RectTransform rectTransform;
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rectTransform.localScale = new Vector3(Screen.width / originalSize.x, Screen.height / originalSize.y, 1);
+    }
+}
